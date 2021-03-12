2e25a36781380e2ea7afe4d375fa2ba320ff5eea
sifubro <theokasio@gmail.com>
2021-03-12 16:27:16 +0000

    HARD RESET TO 8.4 LECTURE

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
-# In regression model requirements we added the kaggle command to be installed
-# Here we download the dataset house-prices-advanced-regression-techniques and saving the dataset in to the directory packages/regression_model/regression_model/datasets/
-# This was the directory that we manually saved the train.csv and test.csv. However, these are not stored in the version control,
-# hence circleci will not be able to find them. That's why we run this script, to place those files into our version the cloud.
-# This ensures we have these files available for circleci so that we can run the tests that rely on test data.
-
-# This kaggle command line interface requires a kaggle key. Find it from your kaggle account. Create new API Token.
-# then use circleci environmental variables and save them as KAGGLE_USERNAME and KAGGLE_KEY in the setting -> enviromental variables
-# in order to run the jobs. These jobs will run everytime we open a pull request, we merge to master etc. W
-# We start to automate the process of testing.
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
--- scripts/publish_model.sh
+++ scripts/publish_model.sh
@@ -2,14 +2,13 @@
 
 # Building packages and uploading them to a Gemfury repository
 
-GEMFURY_URL=$GEMFURY_PUSH_URL
+GEMFURY_URL=$PIP_EXTRA_INDEX_URL
 
 set -e
 
 DIRS="$@"
 BASE_DIR=$(pwd)
-SETUP="setup.py"  # We specify where the setup.py file is
-# we are doing this in 
+SETUP="setup.py"
 
 warn() {
     echo "$@" 1>&2
@@ -27,12 +26,7 @@ build() {
     [ ! -e $SETUP ] && warn "No $SETUP file, skipping" && return
     PACKAGE_NAME=$(python $SETUP --fullname)
     echo "Package $PACKAGE_NAME"
-
-    # Here we generate the distibution
-    python "$SETUP" sdist bdist_wheel || die "Building package $PACKAGE_NAME failed" 
-
-    # We loop over the dist directory /regression_model/dist/  and upload the files found in there to gemfury
-    # e.g. regression_model-0.1.3.tar.gz and regression_model-0.1.3-py3-non-ar
+    python "$SETUP" sdist bdist_wheel || die "Building package $PACKAGE_NAME failed"
     for X in $(ls dist)
     do
         curl -F package=@"dist/$X" "$GEMFURY_URL" || die "Uploading package $PACKAGE_NAME failed on file dist/$X"
