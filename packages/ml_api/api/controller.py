a1b9c7562e78389d3403e9ec84fbb25157060613
sifubro <theokasio@gmail.com>
2021-03-12 16:50:59 +0000

    go back to my defaults

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from regression_model.predict import make_prediction
 from regression_model import __version__ as _version
-from neural_network_model.predict import make_single_prediction
+#from neural_network_model.predict import make_single_prediction
 import os
 from werkzeug.utils import secure_filename
 
@@ -19,7 +19,7 @@ prediction_app = Blueprint('prediction_app', __name__)
 def health():
     if request.method == 'GET':
         _logger.info('health status OK')
-        return 'ok'
+        return '<h1> Hello Theo :) Welcome to the site! </h1>'
 
 
 @prediction_app.route('/version', methods=['GET'])
@@ -52,7 +52,7 @@ def predict():
                         'version': version,
                         'errors': errors})
 
-
+'''
 @prediction_app.route('/predict/classifier', methods=['POST'])
 def predict_image():
     if request.method == 'POST':
@@ -87,3 +87,9 @@ def predict_image():
         return jsonify(
             {'readable_predictions': readable_predictions[0],
              'version': version})
+'''
+
+
+
+
+             
